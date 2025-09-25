d5b3ad8e6b63c31e13547a757c8d6ad9000a4fa1
Andrew Sasmito <Andrew.Sasmito@ibm.com>
2025-09-25 14:16:04 -0400

    Add better prompting to try to increase AI accuracy
    Signed-off-by: Andrew Sasmito <Andrew.Sasmito@ibm.com>

--- src/main/java/io/openliberty/tools/common/ai/tools/AgentTools.java
+++ src/main/java/io/openliberty/tools/common/ai/tools/AgentTools.java
@@ -12,6 +12,7 @@ public class AgentTools implements ToolInterface {
 
     Assistant assistant;
     CodingTools codingTools;
+    OpenLibertyTools openLibertyTools = new OpenLibertyTools();
     
     public AgentTools(CodingTools codingTools) {
         this.codingTools = codingTools;
@@ -22,27 +23,37 @@ public class AgentTools implements ToolInterface {
     }
 
     @Tool("Enable an openliberty feature")
-    public String queryAgent(@P("What you would like to do") String query) throws Exception {
+    public String enableOpenLibertyFeature(@P("What you would like to do") String query) throws Exception {
         // Use a different Agent to allow concurrent messaging, we can generalize this to any multifile update by having an agent at the beginning
         // return a list of files that need to update based on the query
+
+        assistant.chat(AGENT_ID, "Here are all the installable openliberty features" + openLibertyTools.getInstallableLibertyFeatures(null));
 
         String pomXMLContent = codingTools.readFile("pom.xml");
         String newPomXML = assistant.chat(AGENT_ID,
             "With the following query given by the user: " + query + "\n" +
             "Here is the old pom.xml\n" + pomXMLContent + "\n" +
-            "Update the pom.xml and return ONLY the new pom.xml without any codeblocks (Markdown)"
+            "Update the pom.xml and return ONLY all the contents from start to beginning of the new pom.xml without any codeblocks (Markdown).\n" +
+            "Ensure the new code can run properly as this code will be injected straight into the project and fulfills what the user wants\n"
         ).content();
 
 
         String serverXMLContent = codingTools.readFile("server.xml");
         String newServerXML = assistant.chat(AGENT_ID,
             "With the following query given by the user: " + query + "\n" +
-            "Here is the old server.xml\n" + serverXMLContent + "\n" +
-            "Update the server.xml and return ONLY the new server.xml without any codeblocks (Markdown)"
+            "Here is the old server.xml: \n" + serverXMLContent + "\n" +
+            "Update the server.xml and return ONLY all the contents from start to beginning of the new server.xml without any codeblocks (Markdown).\n" +
+            "Ensure the new code can run properly as this code will be injected straight into the project and fulfills what the user wants\n"
         ).content();
 
+        if (!assistant.chat(AGENT_ID, "Will the new server.xml and pom.xml work? reply with only [y | n]").content().equalsIgnoreCase("y")) {
+            String explanation = assistant.chat(AGENT_ID, "Why won't it work").content();
+            assistant.evictChatMemory(AGENT_ID);
+            return explanation;
+        }
+
         String agenticResponse = assistant.chat(AGENT_ID, "What changes did you make to enable those features").content();
-        assistant.evictChatMemory(query);
+        assistant.evictChatMemory(AGENT_ID);
 
         codingTools.rewriteFile("pom.xml", newPomXML);
         codingTools.rewriteFile("server.xml", newServerXML);
